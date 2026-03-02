a6b109bc8eefaa02e406e6f89ef0945a02b7ecd9
dashunya17 <d.5ysueva@yandex.ru>
2026-03-02 13:01:23 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace Forms3
+{
+    partial class Form1
+    {
+        private ListBox listBox;
+        private Label lblEven;
+        private Label lblOdd;
+        private Button btnGen;
+        private Button btnCount;
+
+        private void InitializeComponent()
+        {
+            this.btnGen = new Button();
+            this.listBox = new ListBox();
+            this.btnCount = new Button();
+            this.lblEven = new Label();
+            this.lblOdd = new Label();
+
+            // btnGen
+            this.btnGen.Text = "Сгенерировать";
+            this.btnGen.Location = new System.Drawing.Point(20, 20);
+            this.btnGen.Size = new System.Drawing.Size(150, 30);
+            this.btnGen.Click += new EventHandler(this.rand);
+
+            // listBox
+            this.listBox.Location = new System.Drawing.Point(20, 60);
+            this.listBox.Size = new System.Drawing.Size(340, 150);
+
+            // btnCount
+            this.btnCount.Text = "Подсчитать";
+            this.btnCount.Location = new System.Drawing.Point(20, 220);
+            this.btnCount.Size = new System.Drawing.Size(150, 30);
+            this.btnCount.Click += new EventHandler(this.Click);
+
+            // lblEven
+            this.lblEven.Text = "Четных: ";
+            this.lblEven.Location = new System.Drawing.Point(20, 260);
+            this.lblEven.Size = new System.Drawing.Size(150, 20);
+            this.lblEven.ForeColor = System.Drawing.Color.Green;
+
+            // lblOdd
+            this.lblOdd.Text = "Нечетных: ";
+            this.lblOdd.Location = new System.Drawing.Point(20, 285);
+            this.lblOdd.Size = new System.Drawing.Size(150, 20);
+            this.lblOdd.ForeColor = System.Drawing.Color.Red;
+
+            // Form1
+            this.Text = "Четные и нечетные";
+            this.ClientSize = new System.Drawing.Size(400, 320);
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.Controls.Add(this.btnGen);
+            this.Controls.Add(this.listBox);
+            this.Controls.Add(this.btnCount);
+            this.Controls.Add(this.lblEven);
+            this.Controls.Add(this.lblOdd);
+        }
+    }
+}
--- Form1.cs
+++ Form1.cs
@@ -1,64 +1,50 @@
-using System;
-using System.Drawing;
 using System.Windows.Forms;
 
-namespace _1._1
+namespace Forms3
 {
     public partial class Form1 : Form
     {
-            private ListBox listBox;
-            private Label lblEven, lblOdd;
-            private int[] array;
+        private int[] numbers;
 
-            public Form1()
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void rand (object sender, EventArgs e)
+        {
+            numbers = new int[15];
+            listBox.Items.Clear();
+            Random rand = new Random();
+
+            for (int i = 0; i < 15; i++)
             {
-                this.Text = "Задача 3: Четные и нечетные";
-                this.Size = new Size(400, 350);
-
-                Button btnGen = new Button() { Text = "Сгенерировать", Location = new Point(20, 20), Size = new Size(150, 30) };
-                btnGen.Click += BtnGen_Click;
-
-                listBox = new ListBox() { Location = new Point(20, 60), Size = new Size(340, 150) };
-
-                Button btnCount = new Button() { Text = "Подсчитать", Location = new Point(20, 220), Size = new Size(150, 30) };
-                btnCount.Click += BtnCount_Click;
-
-                lblEven = new Label() { Text = "Четных: ", Location = new Point(20, 260), Size = new Size(150, 20), ForeColor = Color.Green };
-                lblOdd = new Label() { Text = "Нечетных: ", Location = new Point(20, 285), Size = new Size(150, 20), ForeColor = Color.Red };
-
-                this.Controls.Add(btnGen);
-                this.Controls.Add(listBox);
-                this.Controls.Add(btnCount);
-                this.Controls.Add(lblEven);
-                this.Controls.Add(lblOdd);
-            }
-
-            private void BtnGen_Click(object sender, EventArgs e)
-            {
-                array = new int[15];
-                Random rnd = new Random();
-                listBox.Items.Clear();
-
-                for (int i = 0; i < 15; i++)
-                {
-                    array[i] = rnd.Next(10, 51);
-                    listBox.Items.Add($"a[{i}] = {array[i]}");
-                }
-            }
-
-            private void BtnCount_Click(object sender, EventArgs e)
-            {
-                if (array == null) return;
-
-                int even = 0, odd = 0;
-                foreach (int num in array)
-                {
-                    if (num % 2 == 0) even++;
-                    else odd++;
-                }
-
-                lblEven.Text = $"Четных: {even}";
-                lblOdd.Text = $"Нечетных: {odd}";
+                numbers[i] = rand.Next(10, 51);
+                listBox.Items.Add($"a[{i}] = {numbers[i]}");
             }
         }
+
+        private void Click(object sender, EventArgs e)
+        {
+            if (numbers == null)
+            {
+                MessageBox.Show("Сначала сгенерируйте массив!");
+                return;
+            }
+
+            int even = 0;
+            int odd = 0;
+
+            foreach (int num in numbers)
+            {
+                if (num % 2 == 0)
+                    even++;
+                else
+                    odd++;
+            }
+
+            lblEven.Text = $"Четных: {even}";
+            lblOdd.Text = $"Нечетных: {odd}";
+        }
     }
+}
